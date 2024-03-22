06e3b4a812e7312ac9a4b26ccb9a0b583c842454
walterXXXXX <icsystems@mail.ru>
2024-03-22 22:40:19 +0300

    Add Lesson 03 Task 1,2

---
+++ Lesson 03/Task1/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <exception>
+#include <future>
+
+template<typename Iterator>
+void findMin(Iterator begin, Iterator end, std::promise<Iterator> pr) {
+	if (begin == end - 1) {
+		pr.set_value(begin);
+		return;
+	}
+	Iterator cur = begin;
+	Iterator min = cur;
+	while (cur != end) {
+		if (*cur < *min)
+			min = cur;
+		cur++;
+	}
+	pr.set_value(min);
+}
+
+template<typename Iterator>
+void mySort(Iterator begin, Iterator end) {
+	if (begin == end)
+		return;
+	for (auto it = begin+1; it < end; it++) {
+		std::promise<Iterator> promise;
+		std::future<Iterator> future = promise.get_future();
+		auto res = std::async(findMin<Iterator>, it, end, std::move(promise));
+		auto min = future.get();
+		if (*(it - 1) > *min)
+			std::swap(*(it - 1), *min);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	// задаем вектор случайных элементов
+	const size_t count = 50;
+	std::vector<int> elements(count);
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(0, 100);
+	for (auto& el : elements)
+		el = dist(gen);
+
+	// исходный вектор
+	std::cout << "Исходный вектор:\n";
+	for (const auto& el : elements) {
+		std::cout << el << "\t";
+	}
+	std::cout << std::endl << std::endl;
+
+	// сортировка
+	mySort(elements.begin(), elements.end());
+
+	// отсортированный вектор
+	std::cout << "Отсортированный вектор:\n";
+	for (const auto& el : elements) {
+		std::cout << el << "\t";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ Lesson 03/Task2/main.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <exception>
+#include <future>
+#include <mutex>
+
+// фукцния заполнения случайными целыми значениями из диапазона [min; max]
+template<typename Iterator>
+void randomEach(Iterator first, Iterator last) {
+	const int min = 0;
+	const int max = 1'000'000;
+	std::random_device rd;
+	std::mt19937 gen{ rd() };
+	std::uniform_int_distribution<> dis(min, max);
+	for (auto& it = first; it < last; it++) {
+		*it = dis(gen);
+	}
+}
+
+// функция вывода не печать
+template<typename Iterator>
+void printEach(Iterator first, Iterator last) {
+	for (auto& it = first; it < last; it++) {
+		std::cout << *it << "\t";
+	}
+}
+
+// функция извлечения квадратного корня
+template<typename Iterator>
+void sqrtEach(Iterator first, Iterator last) {
+	for (auto& it = first; it < last; it++) {
+		if (*it < 0)
+			throw (std::exception("Ошибка извлечения корня из отрицательного числа"));
+		*it = std::sqrt(*it);
+	}
+}
+
+template<typename Iterator, typename Function>
+void para_for_each(Iterator first, Iterator last, Function func, std::launch launchType = std::launch::async) {
+	const size_t size = 25;
+	auto length = std::distance(first, last);
+	if (length <= size) {
+		func(first, last);
+		return;
+	}
+	std::vector<int>::iterator mid = first;
+	std::advance(mid, length / 2);
+	std::future<void> ft = std::async(launchType, func, first, mid);
+	if (launchType == std::launch::deferred)
+		ft.wait();
+	func(mid, last);
+} 
+
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	std::vector<int> vec(100);
+	para_for_each(vec.begin(), vec.end(), randomEach<std::vector<int>::iterator>);
+	para_for_each(vec.begin(), vec.end(), printEach<std::vector<int>::iterator>, std::launch::deferred);
+	std::cout << "\n\n";
+
+	para_for_each(vec.begin(), vec.end(), sqrtEach<std::vector<int>::iterator>);
+	para_for_each(vec.begin(), vec.end(), printEach<std::vector<int>::iterator>, std::launch::deferred);
+	std::cout << "\n\n";
+
+	para_for_each(vec.begin(), vec.end(), [](auto first, auto last) {
+		for (auto& it = first; it < last; it++) 
+			(*it)++; 
+		});
+
+	para_for_each(vec.begin(), vec.end(), printEach<std::vector<int>::iterator>, std::launch::deferred);
+	std::cout << "\n\n";
+
+	return 0;
+}
+
